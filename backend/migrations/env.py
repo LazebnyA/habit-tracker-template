a3d05b295626376a45ce74b97c6886080467243b
oleksandr <oleksandrlazebnij8@gmail.com>
2024-05-27 11:37:09 +0300

    deploy test1

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from config import DB_URL
 from models.database import Base
 
 # this is the Alembic Config object, which provides
@@ -13,11 +13,7 @@ from models.database import Base
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_PASS", DB_PASS)
+config.set_section_option(section, "DB_URL", DB_URL)
 
 
 if config.config_file_name is not None:
